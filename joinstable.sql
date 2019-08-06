b732d793aa0bfbbea01a2a18ed195110693674b2
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-08-06 14:02:52 -0400

    joins table

---
+++ joinstable.sql
@@ -0,0 +1,16 @@
+SELECT
+    first_name,
+    last_name,
+    rating
+FROM reviewers
+INNER JOIN reviews
+    ON reviewers.id = reviews.reviewer_id;
+    
+
+SELECT
+    first_name,
+    last_name,
+    rating
+FROM reviews
+INNER JOIN reviewers
+    ON reviewers.id = reviews.reviewer_id;
