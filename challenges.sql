0a4f3faea7960c5c62a5296328024c1721e8d652
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-08-06 14:30:29 -0400

    update challenges

--- challenges.sql
+++ challenges.sql
@@ -26,3 +26,16 @@ SELECT
         '!'
     ) AS yell
 FROM books ORDER BY author_lname;
+
+
+SELECT first_name, 
+       last_name, 
+       Count(rating)                               AS COUNT, 
+       Ifnull(Min(rating), 0)                      AS MIN, 
+       Ifnull(Max(rating), 0)                      AS MAX, 
+       Round(Ifnull(Avg(rating), 0), 2)            AS AVG, 
+       IF(Count(rating) > 0, 'ACTIVE', 'INACTIVE') AS STATUS 
+FROM   reviewers 
+       LEFT JOIN reviews 
+              ON reviewers.id = reviews.reviewer_id 
+GROUP  BY reviewers.id; 
